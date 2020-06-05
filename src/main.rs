870d7916f2618b55c6ce122f4255288d50821864
Joe <joe.morgan@bottomline.com>
2020-06-05 08:18:03 -0400

    build struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+struct Mood<'a>{
+    activity: &'a str,
+    rating: &'a str,
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mood: Mood = Mood { activity: &args[1], rating: &args[2]};
+    //let activity = &args[1];
+    //let rating = &args[2];
+
+    println!("You are doing {}", mood.activity);
+    println!("And feel {}", mood.rating);
 }
