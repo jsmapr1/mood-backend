f055a490b30f667ac7d0cc1956e58075eac02b72
Joe <joe.morgan@bottomline.com>
2020-06-12 12:56:29 -0400

    return json from file as api

--- src/employees/routes.rs
+++ src/employees/routes.rs
@@ -1,4 +1,10 @@
 use actix_web::{get, web, HttpResponse, Responder};
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
 // This doesn't seem right. Figure out how to
 // use local files
 use crate::employees::model::Employee;
@@ -9,37 +15,28 @@ async fn index3() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Employee>, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(u)
+}
+
 #[get("/employees")]
 async fn find_all() -> impl Responder {
-    HttpResponse::Ok().json(vec![
-        Employee {
-            id: 1,
-            first_name : "Ola".to_string(),
-            last_name: "John Ajiboye".to_string(),
-           department: "Engineering".to_string(),
-           salary: 4500,
-           age: 23
-        },
-       Employee {
-             id: 2,
-            first_name : "James".to_string(),
-            last_name: "Bond".to_string(),
-           department: "Accounting".to_string(),
-           salary: 3500,
-           age: 43
-        },
-    ])
+    let u = read_user_from_file("./text.json").unwrap();
+    HttpResponse::Ok().json(u)
 }
+
 #[get("/employees/{id}")]
 async fn find() -> impl Responder {
-    HttpResponse::Ok().json(Employee {
-       id: 2,
-            first_name : "James".to_string(),
-            last_name: "Bond".to_string(),
-           department: "Accounting".to_string(),
-           salary: 3500,
-           age: 43
-    })
+    let u = read_user_from_file("./text.json").unwrap();
+    HttpResponse::Ok().json(u.get(0))
 }
 
 pub fn init_routes(config: &mut web::ServiceConfig) {
